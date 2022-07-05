77d1a1a6974cc1f3b8068273da1799ee0b934100
abdellahelaajjouri <abdellah.elaajjouri@gmail.com>
2022-07-05 20:39:07 +0100

    Updating Expenses Chart component
    making a static css style implementation without any dynamic values from Jsondata

--- mon-app/src/components/ExpensesChart.jsx
+++ mon-app/src/components/ExpensesChart.jsx
@@ -1,45 +1,76 @@
 import React, { Component } from "react";
 class ExpensesChart extends Component {
-  state = {};
+  state = {
+    data: [
+      {
+        day: "mon",
+        amount: 17.45,
+      },
+      {
+        day: "tue",
+        amount: 34.91,
+      },
+      {
+        day: "wed",
+        amount: 52.36,
+      },
+      {
+        day: "thu",
+        amount: 31.07,
+      },
+      {
+        day: "fri",
+        amount: 23.39,
+      },
+      {
+        day: "sat",
+        amount: 43.28,
+      },
+      {
+        day: "sun",
+        amount: 25.48,
+      },
+    ],
+  };
   render() {
     return (
       <main className="card__body__content">
         <div className="col1">
-          <div className="bar mon"></div>
+          <div className="bar"></div>
           <p>mon</p>
         </div>
         <div className="col2">
-          <div className="bar tue">
+          <div className="bar">
             <div></div>
           </div>
           <p>tue</p>
         </div>
         <div className="col3">
-          <div className="bar wed">
+          <div className="barO">
             <div></div>
           </div>
           <p>wed</p>
         </div>
         <div className="col4">
-          <div className="bar thu">
+          <div className="bar">
             <div></div>
           </div>
           <p>thu</p>
         </div>
         <div className="col5">
-          <div className="bar fri">
+          <div className="bar">
             <div></div>
           </div>
           <p>fri</p>
         </div>
         <div className="col6">
-          <div className="bar sat">
+          <div className="bar">
             <div></div>
           </div>
           <p>sat</p>
         </div>
         <div className="col7">
-          <div className="bar sun">
+          <div className="bar">
             <div></div>
           </div>
           <p>sun</p>
